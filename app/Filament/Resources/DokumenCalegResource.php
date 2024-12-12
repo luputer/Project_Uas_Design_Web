0b88c9487f431d332791b1d29899ab4c91ff5725
luputer <m.saidibjm4@gmail.com>
2024-12-12 13:39:42 +0800

    mege personal branding, fix resorce dokumen caleg

--- app/Filament/Resources/DokumenCalegResource.php
+++ app/Filament/Resources/DokumenCalegResource.php
@@ -2,16 +2,22 @@
 
 namespace App\Filament\Resources;
 
-use App\Filament\Resources\DokumenCalegResource\Pages;
-use App\Filament\Resources\DokumenCalegResource\RelationManagers;
-use App\Models\DokumenCaleg;
 use Filament\Forms;
-use Filament\Forms\Form;
-use Filament\Resources\Resource;
 use Filament\Tables;
+use Filament\Forms\Form;
 use Filament\Tables\Table;
+use App\Models\DokumenCaleg;
+use Filament\Resources\Resource;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\Toggle;
+use Filament\Forms\Components\Textarea;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\TextInput;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use App\Filament\Resources\DokumenCalegResource\Pages;
+use App\Filament\Resources\DokumenCalegResource\RelationManagers;
 
 class DokumenCalegResource extends Resource
 {
@@ -23,7 +29,12 @@ class DokumenCalegResource extends Resource
     {
         return $form
             ->schema([
-                //
+                Select::make('caleg_id')->relationship('caleg', 'nama_lengkap')
+                    ->required()->label('Caleg'),
+                TextInput::make('jenis_dokumen')->required()->label('Jenis Dokumen'),
+                FileUpload::make('file_path')->required()->label('Path File'),
+                Toggle::make('status_verifikasi')->label('Status Verifikasi'),
+                Textarea::make('catatan')->label('Catatan')->nullable(),
             ]);
     }
 
@@ -31,7 +42,17 @@ class DokumenCalegResource extends Resource
     {
         return $table
             ->columns([
-                //
+                TextColumn::make('caleg.nama_lengkap')->label('Nama Caleg'),
+                TextColumn::make('jenis_dokumen')->label('Jenis Dokumen'),
+                TextColumn::make('file_path')->label('Path File'),
+                TextColumn::make('status_verifikasi')
+                    ->label('Status Verifikasi')
+                    ->getStateUsing(function ($record) {
+                        return $record->status_verifikasi ? 'Terverifikasi' : 'Tidak Terverifikasi';
+                    }),
+                TextColumn::make('catatan')->label('Catatan'),
+                TextColumn::make('created_at')->label('Dibuat Pada')->dateTime(),
+                TextColumn::make('updated_at')->label('Diperbarui Pada')->dateTime(),
             ])
             ->filters([
                 //
