addec60e88579fc0453c73774f9c7d4e33dfaa5b
luputer <m.saidibjm4@gmail.com>
2024-12-12 16:08:18 +0800

    add database

--- app/Http/Controllers/welcomeController.php
+++ app/Http/Controllers/welcomeController.php
@@ -9,7 +9,16 @@ class WelcomeController extends Controller
 {
     public function welcome()
     {
-        $personalBrandData = PersonalBrand::first();
+        $personalBrandData = PersonalBrand::all();  // Mengambil semua data  
+
+    return view('welcome', compact('personalBrandData'));  
+
+        // Cek jika data tidak ditemukan  
+        if (!$personalBrandData) {
+            // Tangani kasus ketika tidak ada data  
+            // Misalnya, Anda bisa mengalihkan pengguna atau menampilkan pesan  
+            return view('welcome')->with('message', 'Tidak ada data merek pribadi ditemukan.');
+        }
 
         return view('welcome', compact('personalBrandData'));
     }
