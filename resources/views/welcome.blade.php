791c488c53770bf5f649e290e0fbcd9b3fe8df34
Arinaldi <arinaldisurya374@gmail.com>
2024-12-13 14:23:03 +0800

    fix

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -222,7 +222,6 @@
                             <div id="slide1" class="carousel-item relative w-full">
                                 <img src="{{ asset('img/project1.png') }}"
                                     class="w-full object-cover rounded-lg" />
-                                <img src="{{ asset('img/project2.png') }}" class="w-full object-cover rounded-lg" />
                                 <div
                                     class="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
                                     <a href="#slide4" class="btn btn-circle bg-gray-800/70 hover:bg-gray-800">❮</a>
@@ -232,7 +231,6 @@
                             <div id="slide2" class="carousel-item relative w-full">
                                 <img src="{{ asset('img/project2.jpg') }}"
                                     class="w-full object-cover rounded-lg" />
-                                <img src="{{ asset('img/project3.jpg') }}" class="w-full object-cover rounded-lg" />
                                 <div
                                     class="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
                                     <a href="#slide1" class="btn btn-circle bg-gray-800/70 hover:bg-gray-800">❮</a>
@@ -242,7 +240,6 @@
                             <div id="slide3" class="carousel-item relative w-full">
                                 <img src="{{ asset('img/project3.jpg') }}"
                                     class="w-full object-cover rounded-lg" />
-                                <img src="{{ asset('img/tes.jpg') }}" class="w-full object-cover rounded-lg" />
                                 <div
                                     class="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
                                     <a href="#slide2" class="btn btn-circle bg-gray-800/70 hover:bg-gray-800">❮</a>
